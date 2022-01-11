c52d55deb0dc68f453a6b3b879f55c4b8c3db6dd
kaizen3031593 <zen.conroy@gmail.com>
2022-01-11 18:24:46 -0500

    move back code

--- lib/my-stage.ts
+++ lib/my-stage.ts
@@ -1,16 +1,28 @@
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as s3 from 'aws-cdk-lib/aws-s3';
-import { App, RemovalPolicy, Stack, Stage, StageProps } from 'aws-cdk-lib';
+import { RemovalPolicy, Stack, Stage, StageProps } from 'aws-cdk-lib';
 
 export class MyStage extends Stage {
   constructor(scope: Stack, id: string, props: StageProps = {}) {
     super(scope, id, props);
 
     const fnStack = new Stack(this, 'FunctionStack');
+    const bucketStack = new Stack(this, 'BucketStack');
+
+    const bucket = new s3.Bucket(bucketStack, 'Bucket', {
+      autoDeleteObjects: true,
+      removalPolicy: RemovalPolicy.DESTROY,
+    });
 
     const fn = new lambda.Function(fnStack, 'Function', {
       code: lambda.Code.fromInline('mycode'),
       handler: 'index.handler',
       runtime: lambda.Runtime.NODEJS_14_X,
+      environment: {
+        BUCKET_NAME: bucket.bucketName, // <-- cross stack reference
+      },
     });
-  }}
+
+    bucket.grantRead(fn);
+  }
+}
