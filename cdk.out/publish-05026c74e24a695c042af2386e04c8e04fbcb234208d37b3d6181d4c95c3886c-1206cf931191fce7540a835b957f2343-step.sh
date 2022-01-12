43a8a3abbaa0f904f3a1caa2ccb42ea0cd0e7395
kaizen3031593 <zen.conroy@gmail.com>
2022-01-12 16:51:30 -0500

    us east 2

---
+++ cdk.out/publish-05026c74e24a695c042af2386e04c8e04fbcb234208d37b3d6181d4c95c3886c-1206cf931191fce7540a835b957f2343-step.sh
@@ -0,0 +1,1 @@
+npx cdk-assets --path "cdk.out/assembly-StageB/StageBBucketStackDF3FFF07.assets.json" --verbose publish "05026c74e24a695c042af2386e04c8e04fbcb234208d37b3d6181d4c95c3886c:489318732371-us-east-2"
