fcd00839aee87b6a342adbc7cb351bef0374ce90
kaizen3031593 <zen.conroy@gmail.com>
2022-01-28 13:34:56 -0500

    test role

--- cdk.out/publish-Assets-FileAsset6-step.sh
+++ cdk.out/publish-Assets-FileAsset6-step.sh
@@ -1,2 +1,2 @@
 set -x
-npx cdk-assets --path "cdk.out/assembly-StageB/StageBFunctionStack18098DCD.assets.json" --verbose publish "531014aa099e9cb12e06249b5e4f64a324c1b4a44f9d1ed6130d215e22eab443:489318732371-eu-west-1"
+npx cdk-assets --path "cdk.out/assembly-StageB/StageBFunctionStack18098DCD.assets.json" --verbose publish "9a91118226505d44e6d73e1f9959589e089e71d81cfdc950ff2ebb7cf9ba5c9e:489318732371-eu-west-1"
