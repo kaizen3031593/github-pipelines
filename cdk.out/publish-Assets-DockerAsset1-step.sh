7eb35c3df9818759220104d22f4273fa1bf3e574
kaizen3031593 <zen.conroy@gmail.com>
2022-02-08 14:31:39 -0500

    new hashes

--- cdk.out/publish-Assets-DockerAsset1-step.sh
+++ cdk.out/publish-Assets-DockerAsset1-step.sh
@@ -1,5 +1,4 @@
 set -x
 npx cdk-assets --path "cdk.out/assembly-StageA/StageAFunctionStackD42C27B8.assets.json" --verbose publish "ddce17e2014f82c113eda5dace24564bea5d971bf6ffab77e2e9f2136fbe23fa:489318732371-us-east-1"
-echo '::set-output name=hash::ddce17e2014f82c113eda5dace24564bea5d971bf6ffab77e2e9f2136fbe23fa'
 npx cdk-assets --path "cdk.out/assembly-StageB/StageBFunctionStack18098DCD.assets.json" --verbose publish "ddce17e2014f82c113eda5dace24564bea5d971bf6ffab77e2e9f2136fbe23fa:489318732371-eu-west-1"
 echo '::set-output name=hash::ddce17e2014f82c113eda5dace24564bea5d971bf6ffab77e2e9f2136fbe23fa'
